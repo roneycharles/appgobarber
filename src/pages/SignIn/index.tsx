e9c595ad86edcc22dcdd101d2cc1eaa09406a248
roneycharles <mmn.charles@gmail.com>
2020-07-27 13:56:22 -0300

    create buttom/input

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu login</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {
+        console.log('deu');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
+
+export default SignIn;
