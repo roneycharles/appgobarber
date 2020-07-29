b9aa8154b70f1b9fa4996e56056397750d1151c4
roneycharles <mmn.charles@gmail.com>
2020-07-29 20:19:51 -0300

    create forms/forms-styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import  React from 'react';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SignIn from '../pages/SignIn';
@@ -11,7 +11,8 @@ const AuthRoutes: React.FC = () => (
     screenOptions={{
       headerShown: false,
       cardStyle: { backgroundColor: '#312e38' },
-    }}>
+    }}
+  >
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
   </Auth.Navigator>
